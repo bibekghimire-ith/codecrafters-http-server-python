8e8e7bfd15c7778786c34ef5cabe6d0897ba7892
bibekghimire-ith <bibek.ghimire83@gmail.com>
2024-03-12 21:36:29 +0545

    7th stage -> Get a file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
-
+import os
+import sys
 
 # BUFFER_SIZE = 4096
 BUFFER_SIZE = 65536
 
 ResponseString = str
 ResponseData = str
+ContentType = str
 
 def read_from_socket(sock: socket.socket) -> str:
     data = sock.recv(BUFFER_SIZE)
@@ -35,36 +37,67 @@ def parse_user_agent(data: str) -> str:
         if tokens[0] == "User-Agent:":
             return " ".join(tokens[1:])
         
-                
-def get_resposne(data: str) -> tuple[ResponseString, ResponseData]:
-    path = parse_path(data)
-    
-    if path == "/":
-        return "200 OK", ""  
-    if path.startswith("/echo/"):
-        return "200 OK", path.replace("/echo/", "")
-    if path.startswith("/user-agent"):
-        user_agent = parse_user_agent(data)
-        return "200 OK", user_agent
-    else:
-        return "404 Not Found", ""
-
-def handle_connection(sock: socket.socket) -> None:
-    data = read_from_socket(sock)
-    
-    response_string, response_data = get_resposne(data)
-    response = f"HTTP/1.1 {response_string}\r\nContent-Type: text/plain\r\nContent-Length: {len(response_data)}\r\n\r\n{response_data}"
-
-    send_to_socet(sock, response)
+class HttpHandler:
+    def __init__(self, file_folder_path: str) -> None:
+        self._file_folder_path = file_folder_path
+        
+        
+    def handle_connection(self, sock: socket.socket) -> None:
+        data = read_from_socket(sock)
+        response_string, response_data, content_type = self.get_resposne(data)
+        if content_type:
+            content_type += "\r\n"
+        response = f"HTTP/1.1 {response_string}\r\n{content_type}Content-Length: {len(response_data)}\r\n\r\n{response_data}"
+        send_to_socet(sock, response)
+        
+        
+    def get_resposne(
+        self, data: str
+        ) -> tuple[ResponseString, ResponseData, ContentType]:
+        path = parse_path(data)
+        content_type = ""
+        response_data = ""
+        if path == "/":
+            return "200 OK", response_data, content_type
+        if path.startswith("/echo/"):
+            content_type = "Content-Type: text/plain"
+            response_data = path.replace("/echo/", "")
+            return "200 OK", response_data, content_type
+        if path.startswith("/user-agent"):
+            user_agent = parse_user_agent(data)
+            response_data = user_agent
+            content_type = "Content-Type: text/plain"
+            return "200 OK", response_data, content_type
+        if path.startswith("/files/") and self._file_folder_path:
+            file_name = path.replace("/files/", "")
+            file_path = os.path.join(self._file_folder_path, file_name)
+            if not os.path.exists(file_path) or not os.path.isfile(file_path):
+                return "404 Not Found", response_data, content_type
+            with open(file_path, "r") as file:
+                file_data = file.read()
+                response_data = file_data
+                content_type = "Content-Type: application/octet-stream"
+            return "200 OK", response_data, content_type
+        else:
+            return "404 Not Found", response_data, content_type
+          
 
 
 
 def main():
+    directory = None
+    
+    if len(sys.argv) == 3 and sys.argv[1] == "--directory":
+        directory = sys.argv[2]
+    handler = HttpHandler(directory)
+    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         client_socket, client_address = server_socket.accept()
         
-        threading.Thread(target=lambda: handle_connection(client_socket)).start()
+        threading.Thread(
+            target=lambda: handler.handle_connection(client_socket)
+        ).start()
 
 
 if __name__ == "__main__":
