35d57cf962a7598083aede4c076cfe5bdb751bda
bibekghimire-ith <bibek.ghimire83@gmail.com>
2024-03-12 21:09:47 +0545

    2nd stage -> respond with 2oo

--- app/main.py
+++ app/main.py
@@ -2,6 +2,18 @@
 import socket
 
 
+BUFFER_SIZE = 4096
+
+def read_from_socket(sock: socket.socket) -> None:
+    data = sock.recv(BUFFER_SIZE)
+    print(data.decode())
+    
+    
+def send_to_socet(sock: socket.socket, data: str) -> None:
+    encoded_data = data.encode()
+    sock.send(encoded_data)
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -9,7 +21,11 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    
+    client_socket, client_address = server_socket.accept()
+    read_from_socket(client_socket)
+
+    send_to_socet(client_socket, "HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
